3996ad0ab69f8a837320ea29eb990425fb802f6d
gpestana <g6pestana@gmail.com>
2021-10-13 12:11:09 +0100

    Test env

--- packages/snarkyjs-node/src/index.ts
+++ packages/snarkyjs-node/src/index.ts
@@ -3,7 +3,16 @@ export * from './lib/signature';
 export * from './lib/circuit_value';
 export * from './lib/merkle_proof';
 
+import * as Ex00 from "./ex00_preimage";
+
 import { shutdown } from './snarky';
 if (typeof window === 'undefined') {
   shutdown();
 }
+
+[Ex00].forEach((x, i) => {
+	console.log(`testing exercise ${i}`);
+	//x.test()
+	console.log(i);
+	console.log(x);
+});
