a1b0b58afe51cb3e674f355c38aa472c1bb5c5c4
gpestana <g6pestana@gmail.com>
2021-10-13 12:23:33 +0100

    Adds logging and more tests

--- packages/snarkyjs-node/src/index.ts
+++ packages/snarkyjs-node/src/index.ts
@@ -1,18 +1,27 @@
+console.log("loading dependencies");
+
 export * from './snarky';
 export * from './lib/signature';
 export * from './lib/circuit_value';
 export * from './lib/merkle_proof';
 
 import * as Ex00 from "./ex00_preimage";
-
+import * as Ex000 from "./examples/ex00_preimage";
 import { shutdown } from './snarky';
+
+console.log("starting..");
+
 if (typeof window === 'undefined') {
+	console.log("shutdown()!!");
   shutdown();
 }
 
-[Ex00].forEach((x, i) => {
+console.log("after shutdown()");
+
+[Ex00, Ex000].forEach((x, i) => {
 	console.log(`testing exercise ${i}`);
-	//x.test()
 	console.log(i);
 	console.log(x);
+
+	//x.test()
 });
