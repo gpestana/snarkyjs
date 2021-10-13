56de977cade0e381a47ba1550fc77a27b6fbcb9a
gpestana <g6pestana@gmail.com>
2021-10-13 12:42:45 +0100

    Run tests

--- packages/snarkyjs-node/src/index.ts
+++ packages/snarkyjs-node/src/index.ts
@@ -1,5 +1,3 @@
-console.log("loading dependencies");
-
 export * from './snarky';
 export * from './lib/signature';
 export * from './lib/circuit_value';
@@ -9,19 +7,16 @@ import * as Ex00 from "./ex00_preimage";
 import * as Ex000 from "./examples/ex00_preimage";
 import { shutdown } from './snarky';
 
-console.log("starting..");
-
 if (typeof window === 'undefined') {
 	console.log("shutdown()!!");
   shutdown();
 }
 
-console.log("after shutdown()");
-
 [Ex00, Ex000].forEach((x, i) => {
-	console.log(`testing exercise ${i}`);
-	console.log(i);
+	console.log(`testing exercise ${i}..`);
 	console.log(x);
 
-	//x.test()
+	//x.test();
+
+	console.log(i + " done!");
 });
